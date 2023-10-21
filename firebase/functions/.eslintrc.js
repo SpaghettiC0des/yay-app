aabaaf2a874a6a1700c88fe37bdb8d87c564efc5
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2023-10-22 03:57:45 +0800

    chore(eslint): overrides for *.test.ts

--- firebase/functions/.eslintrc.js
+++ firebase/functions/.eslintrc.js
@@ -1,3 +1,6 @@
+/**
+ * @type {import('eslint').Linter.Config}
+ **/
 module.exports = {
   root: true,
   env: {
@@ -5,6 +8,15 @@ module.exports = {
     node: true,
     'jest/globals': true,
   },
+  overrides: [
+    {
+      files: ['*.test.ts'],
+      rules: {
+        '@typescript-eslint/no-explicit-any': 'off', // Allow any in tests
+        'no-explicit-any': 'off',
+      },
+    },
+  ],
   extends: [
     'eslint:recommended',
     'plugin:import/errors',
@@ -27,5 +39,6 @@ module.exports = {
     'quote-props': ['error', 'as-needed'],
     'import/no-unresolved': 0,
     indent: ['error', 2],
+    'valid-jsdoc': 0,
   },
 };
