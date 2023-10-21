7a5cef8ab29685c3beec0e6ab2f919ba69b10538
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2023-10-22 03:57:46 +0800

    chore: remove unused imports

--- firebase/functions/src/offline.test.ts
+++ firebase/functions/src/offline.test.ts
@@ -1,5 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-// import * as admin from 'firebase-admin';
 // import * as functions from 'firebase-functions-test';
 import {HttpsError} from 'firebase-functions/v2/https';
 import {downloadYoutubeVideo} from '.';
