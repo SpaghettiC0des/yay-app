1c0d3efa89cd2c7dc172bb038a503d3753d17d50
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2023-10-21 15:54:43 +0800

    test: initial unit test

---
+++ firebase/functions/src/offline.test.ts
@@ -0,0 +1,39 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// import * as admin from 'firebase-admin';
+// import * as functions from 'firebase-functions-test';
+import {HttpsError} from 'firebase-functions/v2/https';
+import {downloadYoutubeVideo} from '.';
+
+// const testEnv = functions();
+
+describe('offline', () => {
+  it('should fail when the URL is not valid', async () => {
+    try {
+      await downloadYoutubeVideo(
+        {
+          body: {
+            url: '',
+          },
+        } as any,
+        {} as any,
+      );
+    } catch (e) {
+      expect(e).toEqual(new HttpsError('invalid-argument', 'url is not valid'));
+    }
+  });
+
+  it('should download the video', async () => {
+    return downloadYoutubeVideo(
+      {
+        body: {
+          url: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+        },
+      } as any,
+      {
+        send(data: any) {
+          expect(data).not.toBeNull();
+        },
+      } as any,
+    );
+  });
+});
