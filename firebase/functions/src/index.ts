b91b0e0f0df9f218416c5e6662347ed88560d3a2
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2023-10-22 03:57:47 +0800

    test: provide test bucket name when unit testing

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -2,8 +2,11 @@ import {HttpsError, onRequest} from 'firebase-functions/v2/https';
 import * as admin from 'firebase-admin';
 
 import * as ytdl from 'ytdl-core';
+import {storageBucket} from 'firebase-functions/params';
 
-admin.initializeApp();
+admin.initializeApp({
+  storageBucket: process.env.JEST_WORKER_ID ? 'test' : storageBucket.value(),
+});
 
 export const downloadYoutubeVideo = onRequest(async (request, response) => {
   const {url, options} = request.body as {
