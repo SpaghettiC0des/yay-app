ed3f982f9a10f855af306cf6432bd063c0c14a72
SpaghettiC0des <7194216+SpaghettiC0des@users.noreply.github.com>
2023-11-06 23:25:54 +0800

    fix: hardcode storage bucket

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,12 +1,13 @@
 import * as admin from 'firebase-admin';
 
-import {storageBucket} from 'firebase-functions/params';
+// import {storageBucket} from 'firebase-functions/params';
 
 import serviceAccount from './service-account.json';
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-  storageBucket: process.env.JEST_WORKER_ID ? 'test' : storageBucket.value(),
+  // storageBucket: process.env.JEST_WORKER_ID ? 'test' : storageBucket.value(),
+  storageBucket: 'yay-app-2023.appspot.com',
 });
 
 export * from './youtubedl';
